6e3f7a92f94f4cca5ab2085ad9c8cf3f5b44daea
roger1703 <muraoka.contato@outlook.com>
2025-02-28 23:12:18 -0400

    Commit inicial

---
+++ main.js
@@ -0,0 +1,58 @@
+$(document).ready(function () {
+    $('#carousel-imagens').slick({
+        autoplay: true,
+        arrows: false
+    }),
+$('#telefone').mask('(00) 00000-0000', {
+    placeholder: '(DDD) 12345-6789'
+}),
+
+$('#cpf').mask('000.000.000-00', {
+    placeholder: '123.456.789-00'
+})
+
+$('#cep').mask('00000-000', {
+    placeholder: '012345-678'
+})
+
+$('form').validate({
+    rules: {
+        nome: {
+            required: true
+        },
+        email: {
+        required: true,
+            email: true
+        },
+        telefone: {
+        required: true
+        },
+        endereco: {
+        required: true
+        },
+        cep: {
+        required: true
+        },
+        cpf: {
+        required: true
+        },
+    },
+    messages:{
+        nome :'por favor,insira o seu nome',
+        email:'por favor,insira o seu email',
+        telefone:'por favor,insira o seu telefone',
+        endereco :'por favor,insira o seu endereço',
+        cep :'por favor,insira o seu CEP',
+        cpf :'por favor,insira o seu CPF'
+
+    },
+
+    submitHandler: function (form) {
+        alert("Sua requisição foi enviada para análise, parabéns pela aquisição!");
+        form.reset();
+    },
+    invalidHandler: function (form, validator) {
+        alert("Por favor, preencha os campos para prosseguir com a compra!");
+    }
+})
+})
